2d3b79b1207462c449a9a5c80b12f04c9aed36c1
Ahsien Chang <hsiaohsien0325@gmail.com>
2021-07-08 18:49:33 +0800

    introduce tailwind

--- tradingbot_fe/src/App.tsx
+++ tradingbot_fe/src/App.tsx
@@ -1,32 +1,24 @@
 import { useEffect } from 'react';
-import './App.scss';
+
 
 function App() {
 
-  const fetchData = async () => {
-    const data = await fetch('/account/all');
-    console.log(data);
-  };
-
   useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch('/account/all');
+      console.log(data);
+    };
     fetchData();
   }, []);
 
-  return <>
-    <div className="info">
-      <h1>Tradingbot</h1>
-      <p>Version 1.0.0</p>
 
-      <h3>OpenAPI spec: <a href="http://35.194.167.78/openapi.json">/openapi.json</a></h3>
-      <h3>API Explorer: <a href="http://35.194.167.78/explorer">/explorer</a></h3>
-    </div>
+  return (
+    <>
+        <h3>OpenAPI spec: <a href="http://35.194.167.78/openapi.json">/openapi.json</a></h3>
+        <h3>API Explorer: <a href="http://35.194.167.78/explorer">/explorer</a></h3>
 
-    <footer className="power">
-      <a href="https://loopback.io" target="_blank">
-        <img src="https://loopback.io/images/branding/powered-by-loopback/blue/powered-by-loopback-sm.png" />
-      </a>
-    </footer>
-  </>;
+    </>
+  );
 }
 
 export default App;
